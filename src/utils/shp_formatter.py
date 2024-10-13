ced59e0234e61f15e9c4bed0e07c8a58b99174ae
Charles Sonnet <charlessonnet881@gmail.com>
2024-10-13 14:07:54 +0200

    Changement shp shp_formatter

--- src/utils/shp_formatter.py
+++ src/utils/shp_formatter.py
@@ -7,7 +7,29 @@ def open_shp(path):
     return data, n
 
 
-def data_to_list(path):
+def reconnaissance_polygon(data, i):
+    """
+    Reconnait si on travaille avec un polygon ou un multipolygon.
+    Facilite la prise en charge et le transfert des donnees vers la BDD.
+
+    ---------------
+    Parameters :
+        path : str, le chemin du fichier.shp à traiter
+
+    ---------------
+    Returns :
+        bool : True si un polygon, False si un multipolygon
+
+    """
+    b = True  # On initialise le booleen a True
+    if len(data[i]["geometry"]["coordinates"]) == 1:
+        return b
+    else:
+        b = False
+        return b
+
+
+def data_to_list_poly_point(path):
     """
     Extrait les données du .shp vers une liste de float
     afin de les transférer plus tard dans une bdd
@@ -28,26 +50,22 @@ def data_to_list(path):
     Liste_X = []
     Liste_Y = []
     for i in range(n):
-        Poly_i = data[i]['geometry']['coordinates']
-        k = len(Poly_i)
-        Liste_Polygone.append(Poly_i)
-        if len(Poly_i) == 2:
-            Liste_X.append(Poly_i[0])
-            Liste_Y.append(Poly_i[1])
+        geometry = data[i]["geometry"]["coordinates"]
+
+        if reconnaissance_polygon(data, i):
+            Liste_Polygone.append(geometry)
+            for point in geometry[0]:
+                Liste_X.append(point[0])
+                Liste_Y.append(point[1])
         else:
-            for j in range(k):
-                print(Poly_i)
-                Liste_X.append(Poly_i[j][0])
-                Liste_Y.append(Poly_i[j][1])
+            for multi_polygon in geometry:
+                for polygon in multi_polygon[0]:
+                    Liste_Polygone.append(polygon)
+                    for point in multi_polygon[0]:
+                        Liste_X.append(point[0])
+                        Liste_Y.append(point[1])
     return Liste_Polygone, Liste_X, Liste_Y
 
-# La fonction marche elle ressort les bons trucs et comme elle traite
-# Les trucs dans l'ordre je pense qu'il y a pas de soucis mais on peut
-# Toujours faire le lien avec la DAO en rajoutant des fonction existe etc,
-# tu me diras quand je pourrai les implementer
 
-# print(
-    # data_to_list(
-    #     "1_DONNEES_LIVRAISON_2024-09-00118"
-    #     + "/ADE_3-2_SHP_UTM22RGFG95_GUF-ED2024-09-18/CHFLIEU_COMMUNE.shp")
-    # )
+P, X, Y = data_to_list_poly_point("1_DONNEES_LIVRAISON_2024-09-00118/ADE_3-2_SHP_RGAF09UTM20_GLP-ED2024-09-18/CANTON.shp")
+

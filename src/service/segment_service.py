b08da44368973048995f6c05b3be4affce5faa77
Saïf Boudabsa <saif@Saifs-MacBook-Air.local>
2024-10-23 12:43:38 +0200

    segment_service

--- src/service/segment_service.py
+++ src/service/segment_service.py
@@ -4,15 +4,11 @@ from business_object.segment import Segment
 
 
 class SegmentService:
-    """Classe contenant les méthodes de service des Segments"""
-
     @log
     def creer(self, point1: Point, point2: Point) -> Segment:
-        """Création d'un segment à partir de deux points"""
         nouveau_segment = Segment(point1, point2)
         return nouveau_segment
 
     @log
     def coupe_a_droite(self, segment: Segment, point: Point) -> int:
-        """Vérifie si un segment coupe à droite d'un point"""
         return segment.coupe_a_droite(point)

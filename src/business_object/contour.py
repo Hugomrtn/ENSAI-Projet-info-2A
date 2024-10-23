3f49e07eb6345c2821acb41386248c14b02bed9e
Saïf Boudabsa <saif@Saifs-MacBook-Air.local>
2024-10-23 15:39:10 +0200

    contour

--- src/business_object/contour.py
+++ src/business_object/contour.py
@@ -2,15 +2,16 @@ from business_object.polygone import Polygone
 
 
 class Contour:
-     """Classe représentant un contour
+    """Classe représentant un contour
     Parameters
     ----------
     polygones_composants: list[Polygone]
     polygones_enclaves: list[Polygone]
     """
 
-    def __init__(self, polygones_composants: list[Polygone],
-                 polygones_enclaves: list[Polygone]):
+    def __init__(
+        self, polygones_composants: list[Polygone], polygones_enclaves: list[Polygone]
+    ):
         self.polygones_composants = polygones_composants
         self.polygones_enclaves = polygones_enclaves
 
